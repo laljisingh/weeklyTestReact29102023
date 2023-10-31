c8e73718751ff73a1064e03e764da1e14229a8f8
laljisingh <lalji7411@gmail.com>
2023-10-31 08:40:25 +0530

    component added

---
+++ src/component/LeftPart.js
@@ -0,0 +1,92 @@
+function LeftPart(){
+    return (
+        <div className="left-part">
+                    <div className="image-part">
+                        <img
+                            src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+                            alt="Girl in a jacket"
+                            width="100"
+                            height="100"
+                        />
+                        <h3>Lal Ji Singh</h3>
+                        <p>Hello world!</p>
+                    </div>
+                    <hr></hr>
+                    <div className="descreption-part">
+                        <div className="div-control">
+                            <div className="d1">
+                                <img
+                                    src="https://cdn-icons-png.flaticon.com/128/3889/3889548.png"
+                                    alt="no"
+                                ></img>
+                            </div>
+                            <div className="d2">
+                                <p>age</p>
+                                <p>24 years old</p>
+                            </div>
+                        </div>
+                        <div className="div-control">
+                            <div className="d1">
+                                <img
+                                    src="https://cdn-icons-png.flaticon.com/128/484/484167.png"
+                                    alt="no"
+                                ></img>
+                            </div>
+                            <div className="d2">
+                                <p>Location</p>
+                                <p>24 years old</p>
+                            </div>
+                        </div>
+                        <div className="div-control">
+                            <div className="d1">
+                                <img
+                                    src="https://cdn-icons-png.flaticon.com/128/456/456212.png"
+                                    alt="no"
+                                ></img>
+                            </div>
+                            <div className="d2">
+                                <p>Personality</p>
+                                <p>24 years old</p>
+                            </div>
+                        </div>
+
+                        {/* <div>
+                            <div className="d1">
+                                <img src="https://w7.pngwing.com/pngs/737/36/png-transparent-hourglass-sand-clock-timer-icon-sand-box-glass-angle-gift-box-thumbnail.png"></img>
+                            </div>
+                            <div className="d2">
+                                <p>age</p>
+                                <p>24 years old</p>
+                            </div>
+                        </div>        */}
+                    </div>
+                    <div className="social-icons">
+                        <div className="d1">
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/128/20/20837.png"
+                                alt="no"
+                            ></img>
+                        </div>
+                        <div className="d1">
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png"
+                                alt="no"
+                            ></img>
+                        </div>
+                        <div className="d1">
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/128/174/174855.png"
+                                alt="no"
+                            ></img>
+                        </div>
+                        <div className="d1">
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/128/2111/2111432.png"
+                                alt="no"
+                            ></img>
+                        </div>
+                    </div>
+                </div>
+    );
+}
+export default LeftPart;
